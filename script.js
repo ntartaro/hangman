024b087011d6e1b7994519e68637ab883a089d7c
Nick Tartaro <ntartaro@gmail.com>
2018-09-14 09:48:35 -0400

    more cleaning

--- script.js
+++ script.js
@@ -1,16 +1,15 @@
 //
 // Data storage and input
 //
-var deleter = document.querySelectorAll('.answers')
 var counter = 0														// Gallows status counter
 var flipper = true													// one flippy boi
 var dataStored = []													// Array with input word split into string letters
 const inputField = document.querySelector('.inputField')			// Input field
 const inputButton = document.querySelector('.inputButton')			// Begin button
 var dataField = document.querySelector('.data').value				// Value of input field
-const validAnswers = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', ' ', '-']
+//const validAnswers = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', ' ', '-']
 
-document.querySelector('.y').value = 'Y'
+document.querySelector('.y').value = 'Y'							// Values for game over 
 document.querySelector('.o').value = 'O'
 document.querySelector('.u').value = 'U'
 document.querySelector('.l').value = 'L'
@@ -18,9 +17,9 @@ document.querySelector('.o').value = 'O'
 document.querySelector('.s').value = 'S'
 document.querySelector('.e').value = 'E'
 
-function validateKeyStrokes(val) {					// Passes input from input field
-	var fixedVal = val.toUpperCase()				// Forces uppercase
-	fixedVal = fixedVal.replace('1', '')			// Rejects numbers
+function validateKeyStrokes(val) {									// Passes input from input field
+	var fixedVal = val.toUpperCase()								// Forces uppercase
+	fixedVal = fixedVal.replace('1', '')							// Rejects numbers
 	fixedVal = fixedVal.replace('2', '')
 	fixedVal = fixedVal.replace('3', '')
 	fixedVal = fixedVal.replace('4', '')
@@ -30,10 +29,12 @@ function validateKeyStrokes(val) {					// Passes input from input field
 	fixedVal = fixedVal.replace('8', '')
 	fixedVal = fixedVal.replace('9', '')
 	fixedVal = fixedVal.replace('0', '')
-	return fixedVal									// Sends back fixed input
+	return fixedVal													// Sends back fixed input
 }
 
-// Begin buttona
+//
+// Begin button
+//
 inputButton.addEventListener('click', function(e) {
 
 	e.preventDefault()
@@ -60,7 +61,9 @@ inputButton.addEventListener('click', function(e) {
 	}
 })
 
+//
 // Reset button
+//
 const resetButton = document.querySelector('.rest')								// The reset button
 const remove = document.querySelector('.generatedAnswers')						// Div containing answers
 
@@ -74,16 +77,16 @@ resetButton.addEventListener('click', function(e) {
 			document.querySelectorAll('.alphabet')[i].style.background = '#39ff14'
 			document.querySelectorAll('.alphabet')[i].style.color = 'black'	
 		}
-		while (remove.hasChildNodes()) {  										// Deletes answer boxes
-    		remove.removeChild(remove.firstChild)
-		}
+		// while (remove.hasChildNodes()) {  										// Deletes answer boxes
+  		//   remove.removeChild(remove.firstChild)
+		// }
 	}
 
-	// document.querySelectorAll('.answers').forEach(function (el) {			// Also deletes answer boxes, holding on to it just in case
-	// 		el.parentNode.removeChild(el)
-	// })
+	 document.querySelectorAll('.answers').forEach(function (el) {			// Deletes answer boxes
+	 		el.parentNode.removeChild(el)
+	 })
 
-	document.querySelector('.zero').style.display = 'flex'
+	document.querySelector('.zero').style.display = 'flex'						// Reset Gallows and counter
 	document.querySelector('.one').style.display = 'none'
 	document.querySelector('.two').style.display = 'none'
 	document.querySelector('.three').style.display = 'none'
@@ -96,25 +99,23 @@ resetButton.addEventListener('click', function(e) {
 	counter = 0
 })
 
-
+//
 // Letter buttons
-
-const letterButton = document.querySelector('.letters')
+//
+const letterButton = document.querySelector('.letters')					// Wrapper for letter buttons
 
 letterButton.addEventListener('click', function(e) {
 	e.preventDefault()
 
-	// Stops letters from being clicked if game has not begun
-	if (flipper == true) {
+	if (flipper == true) {												// Stops letters from being clicked if game has not begun
 		return
 	} 
-	// Stops already clicked letters from being clicked again
-	if (e.target.style.color == 'rgb(57, 255, 20)' || e.target.style.background.color == 'black') {	 
-		return
+
+	if (e.target.style.color == 'rgb(57, 255, 20)' || e.target.style == 'black') {	 	// Stops already clicked letters from being clicked again
+		return																
 	}
 
-	// Styles letters after being clicked
-	e.target.style.background = 'black'
+	e.target.style.background = 'black'									// Styles letters after being clicked
 	e.target.style.color = '#39ff14'
 	//e.target.style.border = 'solid 1px #39ff14' (this needs work)
 
@@ -164,21 +165,36 @@ letterButton.addEventListener('click', function(e) {
 
 	// Game over condition
 	if (counter == 8) {
-		document.querySelector('.seven').style.display = 'none'
+		document.querySelector('.seven').style.display = 'none'				// Resets gallows
 		document.querySelector('.eight').style.display = 'flex'
-		document.querySelector('.loser').style.display = 'flex'
+		document.querySelector('.loser').style.display = 'flex'				// Displays game over
 
-		document.querySelectorAll('.answers').forEach(function (el) {
+		document.querySelectorAll('.answers').forEach(function (el) {		// Deletes answer boxes
 			el.parentNode.removeChild(el)
 		})
 		
-		// Black out all letters
-		for (i = 0; i < document.querySelectorAll('.alphabet').length; i ++) {
+		for (i = 0; i < document.querySelectorAll('.alphabet').length; i ++) {		// Black out all letters
 			document.querySelectorAll('.alphabet')[i].style.background = 'black'
 			document.querySelectorAll('.alphabet')[i].style.color = '#39ff14'
 		}
 
-	}							
+	}	
+
+	// document.querySelectorAll('.answers').forEach(function (e) {
+	// 	if (e.value !== '') {
+	// 		e.style.color = '#39ff14'
+	// 		e.style.background = 'black'
+	// 	}
+	// })
+
+	// for (i = 0; i < document.querySelectorAll('.answers').length; i ++) {
+	// 	if (document.querySelectorAll('.answers')[i].value !== '') {
+	// 		for (i = 0; i < document.querySelectorAll('.alphabet').length; i ++) {
+	// 			document.querySelectorAll('.alphabet')[i].style.background = 'black'
+	// 			document.querySelectorAll('.alphabet')[i].style.color = '#39ff14'
+	// 		}
+	// 	}
+	// }
 })
 
 
